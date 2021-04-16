12ffc2335d0ed849456c78d11d4861958fdaec12
Bogdan Cebere <bogdan.cebere@gmail.com>
2021-04-16 16:25:21 +0300

    improved dependency management

---
+++ cmake/seal.cmake
@@ -0,0 +1,18 @@
+include(FetchContent)
+
+set(SEAL_BUILD_DEPS ON)
+set(SEAL_USE_MSGSL ON)
+set(SEAL_USE_INTEL_HEXL ON)
+set(SEAL_THROW_ON_TRANSPARENT_CIPHERTEXT OFF)
+
+FetchContent_Declare(
+  com_microsoft_seal
+  GIT_REPOSITORY https://github.com/microsoft/SEAL
+  GIT_TAG        v3.6.4
+)
+FetchContent_MakeAvailable(com_microsoft_seal)
+
+include_directories(${com_microsoft_seal_SOURCE_DIR}/native/src)
+include_directories(${com_microsoft_seal_SOURCE_DIR}/thirdparty/msgsl-src/include/)
+include_directories(${com_microsoft_seal_SOURCE_DIR}/thirdparty/hexl-src/hexl/include/)
+include_directories(${com_microsoft_seal_BINARY_DIR}/native/src)
